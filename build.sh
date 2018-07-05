e1d2b63956737c298044b770e794cba94a6f57d7
Sergey Pashaev <sergey.pashaev@gmail.com>
2018-07-05 21:11:39 +0300

    add TARGET_CPU arg to build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p build/ && cd build/
-#cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .. # gcc
-CC=clang CXX=clang++ cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .. # clang
+#cmake -DTARGET_CPU=amd64 -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..  # gcc
+CC=clang CXX=clang++ cmake -DTARGET_CPU=amd64 -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .. # clang
 make -j4
 cd ../
