900c0e4374ce83168044d6eab5a47767e134fc2d
Sergey Pashaev <sergey.pashaev@gmail.com>
2018-08-20 13:57:16 +0300

    s5server: restart server on exceptions

--- src/s5main.cpp
+++ src/s5main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
 #include <boost/asio.hpp>
+#include <boost/asio/signal_set.hpp>
+#include <boost/log/trivial.hpp>
 
 #include "s5server.h"
+
+void run(boost::asio::io_service& io, uint16_t port) {
+    boost::asio::ip::tcp::endpoint ep(boost::asio::ip::tcp::v4(), port);
+    socks5::Server s{io, ep};
+    io.run();
+}
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -10,15 +18,23 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    try {
-        boost::asio::io_service io;
-        boost::asio::ip::tcp::endpoint ep(
-            boost::asio::ip::tcp::v4(),
-            static_cast<uint16_t>(std::atoi(argv[1])));
-        socks5::Server s{io, ep};
-        io.run();
-    } catch (std::exception& e) {
-        std::cerr << "Exception: " << e.what() << '\n';
-    }
+    const uint16_t port = std::atoi(argv[1]);
+    boost::asio::io_service io;
+
+    // stop io service on SIGINT/SIGTERM
+    boost::asio::signal_set signals(io, SIGINT, SIGTERM);
+    signals.async_wait(
+        [&io](const boost::system::error_code&, int) { io.stop(); });
+
+    // restart server until io service isn't stopped & log exceptions
+    do {
+        try {
+            run(io, port);
+        } catch (std::exception& e) {
+            BOOST_LOG_TRIVIAL(error) << "exception: " << e.what();
+            BOOST_LOG_TRIVIAL(info) << "restart server...";
+        }
+    } while (!io.stopped());
+
     return 0;
 }
